4a5a7af411f10b0b7460e4df56bbf1fb4b44c4bd
SUHYUNSHIM <lisa9963suhyun@gmail.com>
2021-07-30 18:45:16 +0900

    teltable5

---
+++ WebPage7/src/hr.sql
@@ -0,0 +1,18 @@
+drop table TelTable5;
+create table TelTable5 (
+  id  number(5),
+  name  varchar2(20),
+  tel   varchar2(20),
+  d   date
+);
+
+insert into TELTABLE5
+values (1,'hong','010-7777-8888','20160201');
+
+insert into TELTABLE5
+values (2,'park','010-3333-5555','20160207');
+
+
+commit;
+
+select * from teltable5
